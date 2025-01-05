07203e341d0e55991c14fac1121f1dff0b009501
Christian Dataro <christian@phrc.live>
2025-01-06 03:45:27 +0800

    feat: ✨  tested

--- index.js
+++ index.js
@@ -208,19 +208,50 @@ function printStats(user) {
 // obj.increment.call(otherObj)
 // console.log("otherObj: " + otherObj.num)
 
-function userCreator(name, score) {
-    this.name = name;
-    this.score = score;
+// function userCreator(name, score) {
+//     this.name = name;
+//     this.score = score;
+// }
+//
+// userCreator.prototype.sayName = function() {
+//     console.log(`I'm ${this.name}`)
+// }
+//
+// userCreator.prototype.increment = function() {
+//     this.score++;
+// }
+//
+// const user1 = new userCreator("Phil", 5);
+// const user2 = new userCreator("Tim", 4);
+// user1.sayName()
+
+class userCreator {
+    constructor(name, score) {
+        this.name = name;
+        this.score = score;
+    }
+    sayName() {
+        console.log(`I am ${this.name}`)
+    }
+    increment() {
+        this.score++;
+    }
 }
 
-userCreator.prototype.sayName = function() {
-    console.log(`I'm ${this.name}`)
-}
-
-userCreator.prototype.increment = function() {
-    this.score++;
-}
-
-const user1 = new userCreator("Phil", 5);
-const user2 = new userCreator("Tim", 4);
+const user1 = new userCreator("Phil", 4);
+const user2 = new userCreator("Tim", 5);
 user1.sayName()
+
+class paidUserCreator extends userCreator {
+    constructor(paidName, paidScore, accountBalance) {
+        super(paidName, paidScore);
+        this.accountBalance = accountBalance;
+    }
+    increaseBalance() {
+        this.accountBalance++;
+    }
+}
+
+const paidUser1 = new paidUserCreator("Alysssa", 8, 25);
+paidUser1.increaseBalance();
+paidUser1.sayName()
